de6213d9312f8dc5ed0b4f0a6dcc3461b2ad85b0
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-12 21:54:56 -0500

    testing locally, changed back to render url

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const routes = require('express').Router();
 const contactsRouter = require('../routes/contacts');
-// const lesson1Controller = require('../controllers/lesson1');
+const lesson1Controller = require('../controllers/lesson1');
 
-// routes.get('/', lesson1Controller.karlaRoute);
-// routes.get('/chance', lesson1Controller.chanceRoute);
-
+routes.get('/', lesson1Controller.karlaRoute);
+routes.get('/chance', lesson1Controller.chanceRoute);
 routes.use('/api-docs', require('./swagger'));
 routes.use('/contacts', contactsRouter);
 
