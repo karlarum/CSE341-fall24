124d638823ad205ce4b93410839ce31e644efd63
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-12 22:38:44 -0500

    fixing post and put  requests

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
-const routes = require('express').Router();
-const contactsRouter = require('../routes/contacts');
-routes.use('/api-docs', require('./swagger'));
-routes.use('/contacts', contactsRouter);
+// const routes = require('express').Router();
+// const contactsRouter = require('../routes/contacts');
+// routes.use('/api-docs', require('./swagger'));
+// routes.use('/contacts', contactsRouter);
 
-module.exports = routes;
+// module.exports = routes;
+
+const express = require('express');
+const router = express.Router();
+
+router.use('/', require('./swagger'));
+router.use('/contacts', require('./contacts'));
+
+module.exports = router;
