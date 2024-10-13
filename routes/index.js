d84849ea2cedb973b7e8f547e2b464490785d040
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-12 22:01:08 -0500

    trying to remove GET /chance from api-dos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 const routes = require('express').Router();
 const contactsRouter = require('../routes/contacts');
-// const lesson1Controller = require('../controllers/lesson1');
-
-// routes.get('/', lesson1Controller.karlaRoute);
-// routes.get('/chance', lesson1Controller.chanceRoute);
 routes.use('/api-docs', require('./swagger'));
 routes.use('/contacts', contactsRouter);
 
